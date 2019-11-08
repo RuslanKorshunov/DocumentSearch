cace67d573883e25d9c40e6e81a4566b045baba6
RuslanKorshunov <ruslan.korsh@yandex.ru>
2019-11-08 11:06:28 +0300

    I've created servlet.

---
+++ src/main/java/by/bsuir/documentsearch/servlet/MainServlet.java
@@ -0,0 +1,31 @@
+package by.bsuir.documentsearch.servlet;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet(name="MainServlet", urlPatterns = "/DocumentSearch")
+public class MainServlet extends HttpServlet {
+    @Override
+    public void init() throws ServletException {
+        super.init();
+    }
+
+    @Override
+    public void destroy() {
+        super.destroy();
+    }
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        super.doGet(req, resp);
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        super.doPost(req, resp);
+    }
+}
---
+++ src/main/webapp/page/search.jsp
@@ -0,0 +1,12 @@
+<%@ page contentType="text/html;charset=UTF-8" %>
+<html>
+<head>
+    <title>Search Page</title>
+</head>
+<body>
+<form action="MainServlet" method="post">
+    <input type="text" name="question">
+    <input type="submit" name="button" value="Search">
+</form>
+</body>
+</html>
